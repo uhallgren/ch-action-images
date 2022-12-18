b0b131dff7b9a64738de169bbaa9cc2330dd6659
Ulf Hallgren <uhallgren@users.noreply.github.com>
2022-12-18 22:07:29 +0100

    Even more fixes

--- .github/actions/sagemcom/f5370e/build-sdk/entrypoint.sh
+++ .github/actions/sagemcom/f5370e/build-sdk/entrypoint.sh
@@ -10,7 +10,7 @@ set -o pipefail
 set -o nounset
 set -o xtrace
 
-COMPILED_SDK_TAR=$PWD/INPUT_COMPILED_SDK_TAR
+COMPILED_SDK_TAR=$PWD/${INPUT_COMPILED_SDK_TAR}
 mkdir -p ${COMPILED_SDK_TAR}
 
 mkdir -p workdir
@@ -25,7 +25,7 @@ ls -l ${COMPILED_SDK_TAR}
 
 # Extract SDK tar file and copy to TOPDIR
 cd ${INPUT_SDK_TAR} 
-tar xvf ${INPUT_VERSION}_*.tar.bz2
+tar xvf ${INPUT_SDK_VERSION}*.tar.bz2
 mv SG4*/* ${WORKDIR}
 mv SG4*/.config ${WORKDIR}
 
@@ -47,9 +47,9 @@ ls -l ${COMPILED_SDK_TAR}
 
 # Tar everything
 cd ${WORKDIR}
-tar cfz ${COMPILED_SDK_TAR}/${INPUT_VERSION}_compiled.tgz *
+tar cfz ${COMPILED_SDK_TAR}/${INPUT_SDK_VERSION}_compiled.tgz *
 
-echo "Input data"
+echo "Output data"
 pwd 
 ls -l
 ls -l ${INPUT_SDK_TAR}
