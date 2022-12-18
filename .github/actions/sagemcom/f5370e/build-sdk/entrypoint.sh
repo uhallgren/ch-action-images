3eb94f786f935448e5dad5c26eaa8d904319c3a7
Ulf Hallgren <uhallgren@users.noreply.github.com>
2022-12-18 19:47:27 +0100

    Try to add action for building sdk.

---
+++ .github/actions/sagemcom/f5370e/build-sdk/entrypoint.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# inputs:
+# - name: sagem-sdk-tar
+
+# outputs:
+# - name: sagem-compiled-sdk-tar
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+
+if [ ! -z "$VERSION" ]; then
+    VERSION="_${INPUT_VERSION}"
+fi
+
+
+SAGEM_SDK_TAR=$INPUT_SDK_TAR
+SAGEM_COMPILED_SDK_TAR=$INPUT_COMPILED_SDK_TAR 
+mkdir -P $SAGEM_COMPILED_SDK_TAR
+
+mkdir -p workdir
+WORKDIR=$PWD/workdir
+
+
+# Extract SDK tar file and copy to TOPDIR
+cd ${SAGEM_SDK_TAR} 
+tar xvf ${VERSION}_*.tar.bz2
+mv SG4*/* ${WORKDIR}
+mv SG4*/.config ${WORKDIR}
+
+# Build the SDK
+chown --recursive developer:developer ${WORKDIR}
+cd ${WORKDIR}
+su developer -c make world
+chown --recursive root:root ${WORKDIR}
+
+# Rename to have more uniformed names
+mv ${WORKDIR}/staging_dir/target-sagemcom*     ${WORKDIR}/staging_dir/target
+
+# Tar everything
+cd ${WORKDIR}
+tar cfz ${SAGEM_COMPILED_SDK_TAR}/${VERSION}_compiled_1.tgz *
