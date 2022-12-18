a93fedd90a2c14af4dd1266997d1470928bb09e0
Ulf Hallgren <uhallgren@users.noreply.github.com>
2022-12-18 21:44:30 +0100

    Another try

--- .github/actions/sagemcom/f5370e/build-sdk/entrypoint.sh
+++ .github/actions/sagemcom/f5370e/build-sdk/entrypoint.sh
@@ -11,10 +11,17 @@ set -o nounset
 set -o xtrace
 
 COMPILED_SDK_TAR=$PWD/INPUT_COMPILED_SDK_TAR
+mkdir -p ${COMPILED_SDK_TAR}
 
 mkdir -p workdir
 WORKDIR=$PWD/workdir
 
+echo "Input data"
+pwd 
+ls -l
+ls -l ${INPUT_SDK_TAR}
+ls -l ${WORKDIR}
+ls -l ${COMPILED_SDK_TAR}
 
 # Extract SDK tar file and copy to TOPDIR
 cd ${INPUT_SDK_TAR} 
@@ -31,6 +38,21 @@ chown --recursive root:root ${WORKDIR}
 # Rename to have more uniformed names
 mv ${WORKDIR}/staging_dir/target-sagemcom*     ${WORKDIR}/staging_dir/target
 
+echo "Bfoer tar"
+pwd 
+ls -l
+ls -l ${INPUT_SDK_TAR}
+ls -l ${WORKDIR}
+ls -l ${COMPILED_SDK_TAR}
+
 # Tar everything
 cd ${WORKDIR}
 tar cfz ${COMPILED_SDK_TAR}/${INPUT_VERSION}_compiled.tgz *
+
+echo "Input data"
+pwd 
+ls -l
+ls -l ${INPUT_SDK_TAR}
+ls -l ${WORKDIR}
+ls -l ${COMPILED_SDK_TAR}
+
