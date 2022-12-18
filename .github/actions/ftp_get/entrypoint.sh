330fe7577f88340d41bc50849f892c4dbe3e3768
Ulf Hallgren <uhallgren@users.noreply.github.com>
2022-12-18 18:19:21 +0100

    Added traces

--- .github/actions/ftp_get/entrypoint.sh
+++ .github/actions/ftp_get/entrypoint.sh
@@ -3,8 +3,16 @@
 # Predefined Hostname
 INPUT_HOST="ftpstanis.telia.com"
 
+pwd 
+ls -l 
+
 # Make sure that the local folder exists
 mkdir -p  $INPUT_LOCAL
+
+ls -l 
+
+echo lftp $INPUT_HOST -u $INPUT_USER,$INPUT_PASSWORD -e \
+"mirror --continue --dereference --exclude=^\.git/$ $INPUT_LOCAL $INPUT_REMOTE; quit"
 
 # Perform the mirroring
 lftp $INPUT_HOST -u $INPUT_USER,$INPUT_PASSWORD -e \
