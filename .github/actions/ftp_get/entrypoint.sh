d6329cc846f8dd50b790a352a5174c04e1d89e71
Ulf Hallgren <uhallgren@users.noreply.github.com>
2022-12-18 18:07:22 +0100

    Create the local folder if it does not exists.

--- .github/actions/ftp_get/entrypoint.sh
+++ .github/actions/ftp_get/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -l
 
+# Predefined Hostname
 INPUT_HOST="ftpstanis.telia.com"
+
+# Make sure that the local folder exists
+mkdir -p  $INPUT_LOCAL
+
+# Perform the mirroring
 lftp $INPUT_HOST -u $INPUT_USER,$INPUT_PASSWORD -e \
 "mirror --continue --dereference --exclude=^\.git/$ $INPUT_LOCAL $INPUT_REMOTE; quit"
